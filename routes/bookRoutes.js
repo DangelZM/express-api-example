1ba2fbd190b2d61cfd95488ca1a7c477bd51ef86
ivan <dangelzm@gmail.com>
2016-12-11 20:46:42 +0200

    PATCH method for book and fix save to be async

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -29,33 +29,51 @@ var routes = (Book) => {
 
     });
 
+  bookRouter.use('/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if(err){
+        res.status(500).send(err);
+      } else if (book) {
+        req.book = book;
+        next();
+      } else {
+        res.status(404).send('no book found');
+      }
+    });
+  });
+
   bookRouter.route('/:bookId')
     .get((req, res) => {
-
-      Book.findById(req.params.bookId, (err, book) => {
-        if(err){
-          res.status(500).send(err);
-        } else {
-          res.json(book);
-        }
-      });
-
+        res.json(req.book);
     })
     .put((req, res) => {
-
-      Book.findById(req.params.bookId, (err, book) => {
-        if(err){
+      req.book.title = req.body.title;
+      req.book.author = req.body.author;
+      req.book.genre = req.body.genre;
+      req.book.read = req.body.read;
+      req.book.save((err) => {
+        if(err) {
           res.status(500).send(err);
         } else {
-          book.title = req.body.title;
-          book.author = req.body.author;
-          book.genre = req.body.genre;
-          book.read = req.body.read;
-          book.save();
-          res.json(book);
+          res.json(req.book);
         }
       });
+    })
+    .patch((req, res) => {
+      if(req.body._id)
+        delete req.body._id;
 
+      for (var p in req.body) {
+          req.book[p] = req.body[p];
+      }
+
+      req.book.save((err) => {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.json(req.book);
+        }
+      });
     });
 
   return bookRouter;
